367639bbbfffc430d3453bb7667a8632d4be44ef
hoarf <alan.ficagna@gmail.com>
2016-07-16 10:42:08 -0300

    Fix action_mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,5 +81,5 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'naoresponda@agropecuariaficagna.com'}
-  config.action_mailer.default_mailer_options = {host: 'agropecuariaficagna.com'}
+  config.action_mailer.default_url_options = {host: 'agropecuariaficagna.com'}
 end
