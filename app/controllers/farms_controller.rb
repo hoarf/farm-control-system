0763e5229073bb1dce1cf8a93936cb83396c7744
hoarf <alan.ficagna@gmail.com>
2016-10-20 20:59:33 -0200

    Fix missing farm

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -10,7 +10,7 @@ class FarmsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_farm
-      @farm = Farm.find(params[:id])
+      @farm = current_user.farm
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
