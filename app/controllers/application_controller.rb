c78bd3dafde155ae82cb072311494b35f8a37021
hoarf <alan.ficagna@gmail.com>
2016-10-22 20:35:45 -0200

    Fix error on redirect to /

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :redirect_if_farmless
   layout :resolve_layout
 
   private
 
   def resolve_layout
-
     return 'devise' if action_name == 'farmyourself'
     return 'devise' if controller_name == 'devise'
     return 'devise' if controller_name == 'sessions'
     return 'devise' if controller_name == 'registrations'
     return 'devise' if controller_name == 'passwords'
     return 'devise' if controller_name == 'confirmations'
-
 
     return 'application' if controller_name == 'farms'
     return 'index' if action_name == 'index'
@@ -24,6 +22,18 @@ class ApplicationController < ActionController::Base
     return 'edit' if action_name == 'edit'
 
     return 'application'
+  end
+
+  def redirect_if_farmless
+    if user_signed_in?
+      if current_user.farmless?
+        if action_name != "farmyourself"
+          if request.get?
+            redirect_to getafarm_path
+          end
+        end
+      end
+    end
   end
 
   def after_sign_in_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-
   belongs_to :farm
 
+  def farmless?
+    farm.nil?
+  end
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
