2a11121c645113bd7c168b6e872eb7aac4ed7f1a
hoarf <alan.ficagna@gmail.com>
2016-10-30 10:51:47 -0200

    Fix mpm returning NaN

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -18,21 +18,31 @@ class Inventory < ActiveRecord::Base
 
   def mpms
     c = []
-    c << initial_balance/initial_amount
+    c << safe_div(initial_balance, initial_amount)
     sorted_entries = entries.sort_by(&:date)
     checkins.each do |ci|
       e = entries.eager_load(:fact).where("facts.date <= ?", ci.date)
-      c << e.to_a.sum(&:total)/e.sum(:amount)
+      c << safe_div(e.to_a.sum(&:total), e.sum(:amount))
     end
     c
   end
 
   def mpm
-    mpms.last.nan? ? 0 : mpms.last
+    mpms.last
   end
 
   def balance(date=Date.today)
     initial_balance + checkins.of(date).to_a.sum(&:total) - checkouts.of(date).to_a.sum(&:total)
+  end
+
+  private
+
+  def safe_div(n1,n2)
+    begin
+      n1/n2
+    rescue
+      0
+    end
   end
 
 end
